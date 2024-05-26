77d18897f23b878c5d376e0f0945cdf5c8a26f9d
sdudnik <sdudnik@ukr.net>
2024-05-26 23:02:17 +0300

    fix end of class

--- ruby/gilded_rose.rb
+++ ruby/gilded_rose.rb
@@ -4,55 +4,55 @@ class GildedRose
     @items = items
   end
 
-def update_quality(items)
-  items.each do |item|
-    case item.name
-    when "Aged Brie"
-      update_aged_brie_quality(item)
-    when "Sulfuras"
-      update_sulfuras_quality(item)
-    when "Backstage passes"
-      update_backstage_passes_quality(item)
-    when "Conjured"
-      update_conjured_quality(item)
+  def update_quality(items)
+    items.each do |item|
+      case item.name
+      when "Aged Brie"
+        update_aged_brie_quality(item)
+      when "Sulfuras"
+        update_sulfuras_quality(item)
+      when "Backstage passes"
+        update_backstage_passes_quality(item)
+      when "Conjured"
+        update_conjured_quality(item)
+      else
+        update_standard_quality(item)
+      end
+
+      item.sell_in -= 1
+    end
+  end
+
+  def update_standard_quality(item)
+    item.quality -= 1
+    item.quality -= 1 if item.sell_in < 0
+  end
+
+  def update_aged_brie_quality(item)
+    item.quality += 1
+  end
+
+  def update_sulfuras_quality(item)
+    # Do nothing
+  end
+
+  def update_backstage_passes_quality(item)
+    if item.sell_in <= 10
+      item.quality += 3
+    elsif item.sell_in <= 5
+      item.quality += 2
     else
-      update_standard_quality(item)
+      item.quality -= 1
     end
 
-    item.sell_in -= 1
-  end
-end
-
-def update_standard_quality(item)
-  item.quality -= 1
-  item.quality -= 1 if item.sell_in < 0
-end
-
-def update_aged_brie_quality(item)
-  item.quality += 1
-end
-
-def update_sulfuras_quality(item)
-  # Do nothing
-end
-
-def update_backstage_passes_quality(item)
-  if item.sell_in <= 10
-    item.quality += 3
-  elsif item.sell_in <= 5
-    item.quality += 2
-  else
-    item.quality -= 1
+    item.quality = 0 if item.sell_in < 0
   end
 
-  item.quality = 0 if item.sell_in < 0
+  def update_conjured_quality(item)
+    item.quality -= 2
+    item.quality -= 2 if item.sell_in < 0
+  end
 end
-
-def update_conjured_quality(item)
-  item.quality -= 2
-  item.quality -= 2 if item.sell_in < 0
-end
-
 
 class Item
   attr_accessor :name, :sell_in, :quality
